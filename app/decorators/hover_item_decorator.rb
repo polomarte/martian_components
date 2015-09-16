c678e010412ce6b538c41e35e17ba5b9e96c8efc
Theo Bittencourt <theodrums@gmail.com>
2015-09-16 15:15:29 -0300

    HoverItem - Open link_url with _blank if host doesn't match with the server

--- app/decorators/hover_item_decorator.rb
+++ app/decorators/hover_item_decorator.rb
@@ -2,4 +2,8 @@ class HoverItemDecorator < ComponentDecorator
   def show_modal?
     object.options[:modal] || object.video_id.present?
   end
+
+  def link_url_target_attr
+    URI.parse(link_url).host == ENV['HOST'] ? '_self' : '_blank'
+  end
 end
