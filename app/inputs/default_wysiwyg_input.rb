258c2244989bc92db98aaed99ac38fe23b0211e9
Theo Bittencourt <theodrums@gmail.com>
2015-09-30 17:17:45 -0300

    Add custom Formtastic inputs (ImagesInput and DefaultWysiwygInput)

---
+++ app/inputs/default_wysiwyg_input.rb
@@ -0,0 +1,8 @@
+class DefaultWysiwygInput < Formtastic::Inputs::Wysihtml5Input
+  def input_html_options
+    super.merge({
+      id:       "default_wysiwyg_input_#{SecureRandom.urlsafe_base64(4)}",
+      commands: [:bold, :italic, :underline, :ul, :ol, :link],
+      blocks:   :barebone})
+  end
+end
---
+++ app/inputs/images_input.rb
@@ -0,0 +1,32 @@
+class ImagesInput
+  include ::Formtastic::Inputs::Base
+
+  def to_html
+    images = [builder.object.image || builder.object.images.build]
+
+    builder.semantic_fields_for :images, images do |img_form|
+      inputs_label = object.class.human_attribute_name(img_form.object.kind) rescue nil
+
+      img_form.inputs inputs_label, class: 'inputs image-uploader-inputs' do
+        t = img_form.template
+
+        li_css_classes = 'fileinput-button file input required'
+
+        t.output_buffer << t.content_tag(:li, class: li_css_classes) do
+          output = img_form.s3_file_field :file, class: 'fileinput-button'
+          output << t.content_tag(:p, img_form.object.decorate.form_hint, class: 'inline-hints')
+          output.html_safe
+        end
+
+        img_form.input :remote_file_url, as: :hidden
+        img_form.input :kind, as: :hidden
+        img_form.input :imageable_id, as: :hidden
+        img_form.input :imageable_type, as: :hidden
+
+        if img_form.object.persisted? && options[:with_destroy?]
+          img_form.input(:_destroy, as: :boolean, label: 'Apagar?')
+        end
+      end
+    end
+  end
+end
