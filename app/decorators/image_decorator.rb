9f6ed56c272fcc838c05e854771c6c13a9ea451f
Theo Bittencourt <theodrums@gmail.com>
2016-05-17 14:29:49 -0300

    Add decorators/image_decorator.rb

---
+++ app/decorators/image_decorator.rb
@@ -0,0 +1,22 @@
+class ImageDecorator < Draper::Decorator
+  delegate_all
+  include Draper::LazyHelpers
+
+  def form_hint
+    output = ''
+
+    image_url = if file = object.file.presence
+      file.svg? ? file.url : (file.try(:thumb).try(:url) || file.url)
+    end
+
+    output << image_tag(image_url)
+
+    output << <<-HTML
+      <span class='placeholder'>
+        Arraste ou clique aqui para adicionar uma imagem
+      </span>
+    HTML
+
+    output.html_safe
+  end
+end
