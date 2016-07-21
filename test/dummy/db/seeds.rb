fc241333b05ccf58426d889898fdf6fb43e9427f
yagoazedias <yagoazedias@gmail.com>
2016-07-21 18:31:06 -0300

    Deleted test_decorator.rb and images tests

--- test/dummy/db/seeds.rb
+++ test/dummy/db/seeds.rb
@@ -39,14 +39,14 @@ links = [
         h2: 'Ipsum volutpat consectetur orci metus consequat imperdiet duis integer semper magna.',
         link_url: 'www.google.com.br',
         images: [build_image('image', 'icon.png')]),
-      
+
       KnowMoreLink.create!(
         key: 'app:know_more_link:know_more_link_b',
         h1: "About Us",
         h2: 'Ipsum volutpat consectetur orci metus consequat imperdiet duis integer semper magna.',
         link_url: 'www.google.com.br',
         images: [build_image('image', 'icon.png')]),
-    
+
       KnowMoreLink.create!(
         key: 'app:know_more_link:know_more_link_c',
         h1: "About You",
@@ -63,46 +63,3 @@ KnowMoreLinks.create!(
   items: links,
   images: [build_image('image', 'icon.png')]
 )
-
-=begin
-links = [
-      KnowMoreLink.create!(
-        key: 'app:know_more_link:know_more_link_1',
-        h1: 'Link 1',
-        images: [
-          build_image('background', 'icon.png'),
-          build_image('icon', 'bg.jpg')],
-        form_options: {
-          additional_editable_attrs: [:link_url],
-          disabled_attrs: [:text]}),
-    
-      KnowMoreLink.create!(
-        key: 'app:know_more_link:know_more_link_2',
-        h1: 'Link 2',
-        images: [
-          build_image('background', 'icon.png'),
-          build_image('icon', 'bg.jpg')],
-        form_options: {
-          additional_editable_attrs: [:link_url],
-          disabled_attrs: [:text]}),
-    
-      KnowMoreLink.create!(
-        key: 'app:know_more_link:know_more_link_3',
-        h1: 'Link 3',
-        images: [
-          build_image('background', 'icon.png'),
-          build_image('icon', 'bg.jpg')],
-        form_options: {
-          additional_editable_attrs: [:link_url],
-          disabled_attrs: [:text]})]
-
-KnowMoreLinks.create!(
-  key:          'app:hover_group:highlights',
-  h1:           'Em Destaque',
-  h2:           'Últimas novidades Amanco',
-  links:        links,
-  form_options: {only_links: true},
-  options: {
-    gallery: true,
-})
-=end
