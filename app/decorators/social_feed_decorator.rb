2a389bb56bb2435dc8a966d8a3e4ff61ba9cae98
Theo B <mail@theob.me>
2016-09-09 19:47:23 -0300

    Fix SocialFeedDecorator#published_at

--- app/decorators/social_feed_decorator.rb
+++ app/decorators/social_feed_decorator.rb
@@ -1,6 +1,6 @@
 class SocialFeedDecorator < ComponentDecorator
   def published_at
-    datetime_diff = DateTime.now.utc.to_i - object.published_at.to_i
+    datetime_diff = DateTime.now.utc.to_i - object.published_at.to_datetime.to_i
     diff_in_minutes = (datetime_diff / 1.minute).minutes
     time_ago_in_words(diff_in_minutes.ago)
   end
