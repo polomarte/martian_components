0336e3422e267da50f4bcfebc2c22f39e83daff1
Theo B <mail@theob.me>
2017-01-17 21:51:57 -0200

    Refact ImagesInput to resolve missing field issue

--- app/inputs/images_input.rb
+++ app/inputs/images_input.rb
@@ -2,39 +2,51 @@ class ImagesInput
   include ::Formtastic::Inputs::Base
 
   def to_html
-    images = object.image_kinds.map do |image_kind|
+    builder.semantic_fields_for :images, images do |img_form|
+      @img_form = img_form
+      img_form_inputs
+    end
+  end
+
+  private
+
+  def img_form
+    @img_form
+  end
+
+  def images
+    object.image_kinds.map do |image_kind|
       object.public_send(image_kind) || object.images.build(kind: image_kind)
     end
+  end
 
-    builder.semantic_fields_for :images, images do |img_form|
-      inputs_label = object.class.human_attribute_name(img_form.object.kind) rescue nil
+  def img_form_inputs
+    img_form.inputs img_form_inputs_label, class: 'inputs media-uploader-inputs' do
+      output = ''
 
-      img_form.inputs inputs_label, class: 'inputs media-uploader-inputs' do
-        t = img_form.template.dup
+      output << template.content_tag(:li, class: 'fileinput-button file input required') do
+        out = ''
+        out << img_form.s3_file_field(:file)
+        out << template.content_tag(:p, img_form.object.decorate.form_hint, class: 'inline-hints')
+        out << img_form.hidden_field(:remote_file_url)
 
-        li_css_classes = 'fileinput-button file input required'
-
-        img_form.template.output_buffer << t.content_tag(:li, class: li_css_classes) do
-          output = ''
-          output << img_form.s3_file_field(:file)
-          output << t.content_tag(:p, img_form.object.decorate.form_hint, class: 'inline-hints')
-
-          output << Nokogiri::HTML.fragment(
-            img_form.input :remote_file_url, as: :hidden).css('input').to_s.html_safe
-
-          if img_form.object.persisted?
-            output << t.link_to('Remover', '#', class: 'remove-media-btn')
-            output << Nokogiri::HTML.fragment(
-              img_form.input(:_destroy, as: :hidden)).css('input').to_s.html_safe
-          end
-
-          output.html_safe
+        if img_form.object.persisted?
+          out << template.link_to('Remover', '#', class: 'remove-media-btn')
+          out << img_form.hidden_field(:_destroy)
         end
 
-        img_form.input :kind, as: :hidden
-        img_form.input :imageable_id, as: :hidden
-        img_form.input :imageable_type, as: :hidden
+        out.html_safe
       end
+
+      output << img_form.input(:kind, as: :hidden)
+      output << img_form.input(:imageable_id, as: :hidden)
+      output << img_form.input(:imageable_type, as: :hidden)
+
+      output.html_safe
     end
+  end
+
+  def img_form_inputs_label
+    object.class.human_attribute_name(img_form.object.kind) rescue nil
   end
 end
