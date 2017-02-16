34ba5c8b08854eb13bd81aa227a8af62f8a14aeb
Theo B <mail@theob.me>
2017-02-16 19:02:36 -0200

    Add some common dependecies

--- lib/martian_components.rb
+++ lib/martian_components.rb
@@ -1,6 +1,8 @@
 require 'martian_components/engine'
 require 'devise'
 require 'carrierwave'
+require 'fog'
+require 'mini_magick'
 require 'globalize'
 require 'slim-rails'
 require 'activeadmin'
@@ -8,8 +10,11 @@ require 'draper'
 require 'bootstrap-sass'
 require 'turbolinks'
 require 's3_file_field'
+require 'jquery.fileupload-rails'
 require 'cocoon'
 require 'inline_svg'
+require 'route_translator'
+require 'data_migrate'
 
 module MartianComponents
 end
